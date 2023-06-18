0e407513e6c2f3805ff8a02f56fc1249fc38619b
Acetonerer <bolokan2014@gmail.com>
2023-06-18 23:20:21 +0700

    Добавил содержимое функций

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +9,13 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('API_KEY')
+        yt = build('youtube', 'v3', developerKey=api_key)
+        chan = yt.channels()
+        chan = chan.list(id=self.channel_id, part='snippet, statistics')
+        chann = chan.execute()
+        print(json.dumps(chann, indent=2, ensure_ascii=False))
